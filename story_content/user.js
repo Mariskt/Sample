10ce117abcd6d640800fa479842fbf135ca2aa58
MARIANNE SEIDLER <mariannes@skillup.tech>
2021-03-31 17:05:38 -0700

    Button updates
    Updating buttons

---
+++ story_content/user.js
@@ -0,0 +1,16 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5vV3nZqGzDA":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+alert("Hello!");
+}
+
